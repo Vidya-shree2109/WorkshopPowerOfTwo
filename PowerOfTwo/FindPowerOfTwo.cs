6122931893e0079d139db9766b80387c4b342900
Vidyashree N M <vidyashree2109@gmail.com>
2022-05-18 10:01:06 +0530

    [Vidya] Add : Finding The Power Of Two Values Is Implemented

---
+++ PowerOfTwo/FindPowerOfTwo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfTwo
+{
+    public class FindPowerOfTwo
+    {
+        public void PowerFinding()
+        {
+            Console.Write("Enter the power value for finding 2's power :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine("2" + " " + "^" + " " + i + " " + "=" + " " + Math.Pow(2, i));
+            }
+        }
+    }
+}
---
+++ PowerOfTwo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PowerOfTwo
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome To Power Of Two Program\n");
+            FindPowerOfTwo power = new FindPowerOfTwo();
+            power.PowerFinding();
+        }
+    }
+}
